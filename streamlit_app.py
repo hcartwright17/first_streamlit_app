2544795f9ae0bf0e1727d26e1f2c6ca622ee5a28
hcartwright17 <104158587+hcartwright17@users.noreply.github.com>
2022-05-05 13:09:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 
 
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
