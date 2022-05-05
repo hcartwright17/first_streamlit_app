1c9bf448e82f57835d86e2df3a5d832c74f90a31
hcartwright17 <104158587+hcartwright17@users.noreply.github.com>
2022-05-05 14:05:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,10 +65,10 @@ def insert_row_snowflake(new_fruit):
              return "Thanks for adding " + new_fruit            
 
  
-          my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-          back_from_function = insert_row_snowflake(add_my_fruit)
-          my_cnx.close() 
-          streamlit.text(back_from_function)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+back_from_function = insert_row_snowflake(add_my_fruit)
+my_cnx.close() 
+streamlit.text(back_from_function)
 
 
 
